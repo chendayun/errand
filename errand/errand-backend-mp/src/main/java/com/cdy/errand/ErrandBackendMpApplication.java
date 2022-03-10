91915e411b8eb3022edc1ac74bdc5afe90f3a1ab
chendayun <chendayun@emial.com>
2022-03-10 10:15:45 +0800

    first commit

---
+++ errand/errand-backend-mp/src/main/java/com/cdy/errand/ErrandBackendMpApplication.java
@@ -0,0 +1,13 @@
+package com.cdy.errand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrandBackendMpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrandBackendMpApplication.class, args);
+    }
+
+}
---
+++ errand/errand-backend-mp/src/test/java/com/cdy/errand/ErrandBackendMpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdy.errand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrandBackendMpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
